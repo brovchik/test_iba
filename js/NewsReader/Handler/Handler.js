3f4e76843a519dec362c2321594f098cdfa4d2af
brovchik <brovchik.ne@gmail.com>
2015-02-17 03:03:23 +0300

    added some unworkable scripts

--- js/NewsReader/Handler/Handler.js
+++ js/NewsReader/Handler/Handler.js
@@ -1,3 +1,12 @@
-NewsReader.Handler = function(container) {
+NewsReader.Handler = function() {
+
+}
+
+NewsReader.Handler.prototype = {
+
+    addNewsList: function() {
+        var newsListProvider = new NewsReader.data.NewsServiceProvider('/data');
+        newsListProvider.getNewsHeadlines();
+    }
 
 }
--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,5 @@
-﻿
+﻿var loadButton = $('.newsreader-header-toolbar-load-button');
+var newsList = new NewsReader.ui.NewsListPanel();
+var loadHandler = new NewsReader.Handler();
+
+loadButton.addEventListener('click', loadHandler.addNewsList());
